73fafbb4e2ddac5c1fef6f89eb2853c88433532a
Ed Elensky <elenskyed516@gmail.com>
2021-04-14 16:52:58 +0300

    edit for test ci cd

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello everyone, Hello Universe'
+    return 'Hello everyone, Hello Universe. It is another test CI CD'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
